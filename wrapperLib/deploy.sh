9aeb12c083decfc239afe85d9c12ae700837c9fd
moroz <wertixvost@gmail.com>
2024-06-19 23:24:52 +0200

    Deploy to latest

--- wrapperLib/deploy.sh
+++ wrapperLib/deploy.sh
@@ -1,3 +1,4 @@
 version=$(jq -r '.version' package.json)
 
 rsync ./dist/assets/ root@194.146.25.179:/root/prebid_wrapper/dist/$version/ -r
+rsync ./dist/assets/ root@194.146.25.179:/root/prebid_wrapper/dist/latest/ -r
